638d1043e280c91a51712d6901c1912e25bfe8b5
AFEDev <algirduj@gmail.com>
2022-03-29 20:41:33 +0300

    new

---
+++ dist/app.js
@@ -0,0 +1,24 @@
+const outputText = document.getElementById('atsakymas');
+const calculateUnits = document.getElementById('skaiciuoti');
+const imput = document.getElementById('input');
+const radioToMm = document.getElementById('tomm');
+const radioToInch = document.getElementById('toinch');
+let calculate = (unitsValue) => {
+    let ansver = 0;
+    if (radioToMm.checked) {
+        ansver = parseInt(unitsValue) * 25.4;
+    }
+    if (radioToInch.checked) {
+        ansver = parseInt(unitsValue) / 25.4;
+    }
+    return ansver;
+};
+calculateUnits.addEventListener('click', () => {
+    if (imput.value != "") {
+        outputText.innerHTML = `${calculate(imput.value)}`;
+    }
+    else {
+        outputText.innerHTML = `Iveskite X ir Y reiksmes skaiciais`;
+    }
+});
+console.log(radioToMm.checked);
---
+++ src/app.ts
@@ -0,0 +1,31 @@
+const outputText:HTMLElement = document.getElementById('atsakymas');
+const calculateUnits = document.getElementById('skaiciuoti') as HTMLButtonElement;
+const imput = document.getElementById('input') as HTMLInputElement;
+const radioToMm = document.getElementById('tomm') as HTMLInputElement;
+const radioToInch =  document.getElementById('toinch') as HTMLInputElement;
+
+
+let calculate = (unitsValue:string) => {
+    let ansver:number = 0;
+    if (radioToMm.checked) {
+        ansver = parseInt(unitsValue) * 25.4;
+    }
+    if (radioToInch.checked) {
+        ansver = parseInt(unitsValue) / 25.4;
+    }
+    return ansver;
+}
+
+
+calculateUnits.addEventListener('click', () =>{
+    if (imput.value != "") {
+        outputText.innerHTML = `${calculate(imput.value)}`;
+    }
+         else {
+            outputText.innerHTML = `Iveskite X ir Y reiksmes skaiciais`;
+        }
+       
+})
+
+
+console.log(radioToMm.checked);
